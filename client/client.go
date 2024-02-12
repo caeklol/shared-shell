f635e805f0d28f51596457f95964d5a30d193fda
caeklol <me@caek.dev>
2024-02-12 16:05:41 +0800

    refactor
    refactor + working server

--- client/client.go
+++ client/client.go
@@ -22,6 +22,9 @@ type ClientMessage struct {
 	Type    string
 }
 
+var logs []string = []string{}
+var render_flag bool = true
+
 func main() {
 	conn, err := net.Dial("tcp", "0.0.0.0:4242")
 	if err != nil {
@@ -45,59 +48,9 @@ func main() {
 
 	cmd.Stderr = cmd.Stdout
 
-	logs := []string{}
-	render_flag := true
-
-	// pipe os stdin
-	go func() {
-		scanner := bufio.NewScanner(os.Stdin)
-		for scanner.Scan() {
-			text := scanner.Text()
-			stdin.Write([]byte(text + "\n"))
-			logs = append(logs, "[IN] "+text)
-			render_flag = true
-			message := ClientMessage{Content: text, Type: "stdin"}
-			data, err := json.Marshal(message)
-			if err != nil {
-				fmt.Println("failed to encode message")
-			}
-			conn.Write(data)
-		}
-	}()
-
-	// pipe connection stdin
-	go func() {
-		decoder := json.NewDecoder(conn)
-		for {
-			var m ServerMessage
-			err := decoder.Decode(&m)
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				break
-			}
-
-			stdin.Write([]byte(m.Content + "\n"))
-			logs = append(logs, "[IN] "+m.Content)
-			render_flag = true
-		}
-	}()
-
-	// pipe command stdout
-	go func() {
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			text := scanner.Text()
-			logs = append(logs, "[OUT] "+text)
-			render_flag = true
-			message := ClientMessage{Content: text, Type: "stdout"}
-			data, err := json.Marshal(message)
-			if err != nil {
-				fmt.Println("failed to encode message")
-			}
-			conn.Write(data)
-		}
-	}()
+	go pipeOsStdin(stdin, conn)
+	go pipeConnStdin(stdin, conn)
+	go pipeCommandStdout(stdout, conn)
 
 	err = cmd.Start()
 	if err != nil {
@@ -105,30 +58,80 @@ func main() {
 		return
 	}
 
-	go func() {
-		for {
-			if !render_flag {
-				time.Sleep(time.Millisecond)
-				continue
-			}
-
-			distance_to_limit := len(logs) - tm.Height() + 1
-			if distance_to_limit > 0 {
-				for i := 0; i < distance_to_limit; i++ {
-					logs = logs[1:]
-				}
-			}
-
-			tm.Clear()
-			tm.MoveCursor(0, 1)
-			for _, s := range logs {
-				tm.Println(s)
-			}
-			tm.MoveCursor(0, tm.Height())
-			tm.Flush()
-			render_flag = false
-		}
-	}()
+	go render()
 
 	cmd.Wait()
+}
+
+func pipeOsStdin(stdin io.WriteCloser, conn net.Conn) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		stdin.Write([]byte(text + "\n"))
+		logs = append(logs, "[IN] "+text)
+		render_flag = true
+		message := ClientMessage{Content: text, Type: "stdin"}
+		data, err := json.Marshal(message)
+		if err != nil {
+			fmt.Println("failed to encode message")
+		}
+		conn.Write(data)
+	}
+}
+
+func pipeConnStdin(stdin io.WriteCloser, conn net.Conn) {
+	decoder := json.NewDecoder(conn)
+	for {
+		var m ServerMessage
+		err := decoder.Decode(&m)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			break
+		}
+
+		stdin.Write([]byte(m.Content + "\n"))
+		logs = append(logs, "[IN] "+m.Content)
+		render_flag = true
+	}
+}
+
+func pipeCommandStdout(stdout io.ReadCloser, conn net.Conn) {
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		text := scanner.Text()
+		logs = append(logs, "[OUT] "+text)
+		render_flag = true
+		message := ClientMessage{Content: text, Type: "stdout"}
+		data, err := json.Marshal(message)
+		if err != nil {
+			fmt.Println("failed to encode message")
+		}
+		conn.Write(data)
+	}
+}
+
+func render() {
+	for {
+		if !render_flag {
+			time.Sleep(time.Millisecond)
+			continue
+		}
+
+		distance_to_limit := len(logs) - tm.Height() + 1
+		if distance_to_limit > 0 {
+			for i := 0; i < distance_to_limit; i++ {
+				logs = logs[1:]
+			}
+		}
+
+		tm.Clear()
+		tm.MoveCursor(0, 1)
+		for _, s := range logs {
+			tm.Println(s)
+		}
+		tm.MoveCursor(0, tm.Height())
+		tm.Flush()
+		render_flag = false
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net"
+	"os"
+	"time"
+
+	tm "github.com/buger/goterm"
 )
 
-type Message struct {
+type ServerMessage struct {
+	Content string
+}
+
+type ClientMessage struct {
 	Content string
 	Type    string
 }
 
+var logs []string = []string{}
+var render_flag bool = true
 var connections int = 0
+var conn net.Conn = nil
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:4242")
@@ -23,7 +35,8 @@ func main() {
 
 	defer listener.Close()
 
-	fmt.Println("Server is listening on port 4242")
+	go render()
+	go pipeOsStdin()
 
 	for {
 		conn, err := listener.Accept()
@@ -39,27 +52,52 @@ func main() {
 
 		go handleClient(conn)
 	}
+
 }
 
-func handleClient(conn net.Conn) {
-	defer handleDisconnect(conn)
-	fmt.Println("Client connected")
-	connections += 1
+func pipeOsStdin() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if conn == nil && connections == 0 {
+			logs = append(logs, "[LOG] No client connected")
+		} else {
+			// -- possible race condition -- [A]
+			logs = append(logs, "[IN] "+text)
+			message := ServerMessage{Content: text}
+			data, err := json.Marshal(message)
+			if err != nil {
+				fmt.Println("failed to encode message")
+			}
+			conn.Write(data)
+		}
 
-	decoder := json.NewDecoder(conn)
+		render_flag = true
+	}
+}
+
+func handleClient(c net.Conn) {
+	defer handleDisconnect(conn)
+	logs = append(logs, "[LOG] Client connected")
+	render_flag = true
+	connections += 1
+	conn = c
+
+	decoder := json.NewDecoder(c)
 	for {
-		var m Message
+		var m ClientMessage
 		err := decoder.Decode(&m)
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			break
 		}
+		text := m.Content
 
 		if m.Type == "stdout" {
-			fmt.Printf("[OUT] %s", m.Content)
+			logs = append(logs, "[OUT] "+text)
 		} else if m.Type == "stdin" {
-			fmt.Printf("[IN] %s", m.Content)
+			logs = append(logs, "[IN] "+text)
 		} else {
 			break
 		}
@@ -67,6 +105,33 @@ func handleClient(conn net.Conn) {
 }
 
 func handleDisconnect(conn net.Conn) {
-	fmt.Println("Client disconnected")
+	logs = append(logs, "[LOG] Client disconnected")
+	render_flag = true
 	connections -= 1
+}
+
+func render() {
+
+	for {
+		if !render_flag {
+			time.Sleep(time.Millisecond)
+			continue
+		}
+
+		distance_to_limit := len(logs) - tm.Height() + 1
+		if distance_to_limit > 0 {
+			for i := 0; i < distance_to_limit; i++ {
+				logs = logs[1:]
+			}
+		}
+
+		tm.Clear()
+		tm.MoveCursor(0, 1)
+		for _, s := range logs {
+			tm.Println(s)
+		}
+		tm.MoveCursor(0, tm.Height())
+		tm.Flush()
+		render_flag = false
+	}
 }
